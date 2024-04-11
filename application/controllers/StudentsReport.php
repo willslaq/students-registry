311dcb64919b3f3df82b39432577485ee6a304cf
Willinghan B. Tomaz <willslaqtomaz@gmail.com>
2024-04-11 16:41:54 -0300

    feat(report): added the report grouping students by their classes with the logo

--- application/controllers/StudentsReport.php
+++ application/controllers/StudentsReport.php
@@ -1,29 +1,46 @@
 <?php
+
+class CustomPDF extends FPDF
+{
+    public function Header()
+    {
+        $this->SetFont('Arial', 'B', 20);
+        $this->Cell(0, 10, 'Relatorio de Estudantes por Turma', 0, 1, 'C');
+    }
+
+    public function Footer()
+    {
+        $this->Image('assets/logo.png', 10, 265, 30);
+    }
+}
+
 class StudentsReport extends CI_Controller
 {
     public function __construct()
     {
         parent::__construct();
         $this->load->model('Student_model');
+        $this->load->model('Classes_model');
     }
 
     public function generate()
     {
-        $data['students'] = $this->Student_model->listStudents();
+        $students = $this->Student_model->listStudents();
+        $classes = $this->Classes_model->listClasses();
 
-        $pdf = new FPDF();
-
-        // $pdf->SetHeaderData('logo.png', 30, 'Relatório de Estudantes', 'Classe: ');
-
-        // $pdf->SetFooterData('footer_logo.png', 30);
+        $pdf = new CustomPDF();
 
         $pdf->AddPage();
-        foreach ($data['students'] as $student) {
-            $pdf->SetFont('helvetica', '', 12);
-            $pdf->Cell(0, 10, 'Nome: ' . $student->name, 0, 1);
-            $pdf->Cell(0, 10, 'Nascimento: ' . $student->birth_date, 0, 1);
+        foreach ($classes as $class) {
+            $pdf->SetFont('helvetica', '', 15);
+            $pdf->Cell(0, 10, 'Turma: ' . $class->name, 0, 1);
+            foreach ($students as $student) {
+                if ($student->classes_id == $class->id) {
+                    $pdf->SetFont('helvetica', '', 12);
+                    $pdf->Cell(0, 10, 'Nome: ' . $student->name, 0, 1);
+                }
+            }
         }
-
         $pdf->Output('students_report.pdf', 'D');
     }
 }
