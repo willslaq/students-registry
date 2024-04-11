76362ce936e82a1cbb9b4597681c1633ef047baa
Willinghan B. Tomaz <willslaqtomaz@gmail.com>
2024-04-11 10:21:35 -0300

    feat(students): finished students forms and fixed issue at the framework on url_helper

--- application/controllers/StudentsCreate.php
+++ application/controllers/StudentsCreate.php
@@ -1,27 +1,41 @@
 <?php
-class StudentsCreate extends CI_Controller {
-    public function __construct() {
+class StudentsCreate extends CI_Controller
+{
+    public function __construct()
+    {
         parent::__construct();
         $this->load->model('Student_model');
         $this->load->helper('url');
     }
 
-    public function index() {
+    public function index()
+    {
         $this->load->view('create_students');
     }
 
-    public function create() {
-        log_message('error', 'entreeeeeeeeeeeeeeei');
+    public function create()
+    {
         $name = $this->input->post('name');
         $document = $this->input->post('document');
+        $address = $this->input->post('address');
+        $city = $this->input->post('city');
+        $parent_name = $this->input->post('parent_name');
+        $parent_phone = $this->input->post('parent_phone');
+        $parent_email = $this->input->post('parent_email');
+        $birth_date = $this->input->post('birth_date');
 
-        
         $data = array(
             'name' => $name,
-            'document' => $document
+            'document' => $document,
+            'address' => $address,
+            'city' => $city,
+            'parent_name' => $parent_name,
+            'parent_phone' => $parent_phone,
+            'parent_email' => $parent_email,
+            'birth_date' => $birth_date
         );
         log_message('error', $data);
-        
+
         $this->Student_model->createStudent($data);
 
         redirect('students');
--- application/views/create_students.php
+++ application/views/create_students.php
@@ -1,8 +1,10 @@
 <!DOCTYPE html>
 <html>
+
 <head>
     <title>Cadastro de Produto</title>
 </head>
+
 <body>
     <h1>Cadastro de Produto</h1>
     <form action="<?= base_url('api/students/create') ?>" method="post">
@@ -12,7 +14,27 @@
         <label for="document">Documento</label>
         <input type="text" id="document" name="document" required><br>
 
+        <label for="address">Endereço:</label>
+        <input type="text" name="address" id="address" required><br>
+
+        <label for="city">Cidade:</label>
+        <input type="text" name="city" id="city" required><br>
+
+        <label for="parent_name">Nome do Responsável:</label>
+        <input type="text" name="parent_name" id="parent_name" required><br>
+
+        <label for="parent_phone">Telefone do Responsável:</label>
+        <input type="text" name="parent_phone" id="parent_phone" required><br>
+
+        <label for="parent_email">E-mail do Responsável:</label>
+        <input type="email" name="parent_email" id="parent_email" required><br>
+
+        <label for="birth_date">Data de Nascimento:</label>
+        <input type="date" name="birth_date" id="birth_date" required><br>
+
+
         <input type="submit" value="Cadastrar">
     </form>
 </body>
+
 </html>
